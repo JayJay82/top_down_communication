cee654f36d5f9610de7c4afbcb236c7f92d81f06
Costantino Cavallo <ccavallo@enetworks.it>
2018-11-05 16:57:57 +0100

    validation added

--- src/app/composed/add-edit-post/add-edit-post.component.ts
+++ src/app/composed/add-edit-post/add-edit-post.component.ts
@@ -1,6 +1,7 @@
 import { Post } from './model/post.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { Mode } from './model/mode.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 enum textButtonValue {
   ADD = "ADD",
@@ -15,19 +16,29 @@ enum textButtonValue {
 
 
 export class AddEditPostComponent implements OnInit {
- 
+  form : FormGroup;
   @Input() model : Post;
   @Input() mode : Mode; 
   buttonText : textButtonValue = textButtonValue.ADD; 
   
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     if(this.mode == Mode.EDIT_MODE) {
       this.buttonText = textButtonValue.EDIT;
+      this.form = this.fb.group({
+        title: [this.model.title,Validators.required],
+        content: [this.model.content,Validators.required]
+      });
+    }
+    else {
+      this.form = this.fb.group({
+        title: ["",Validators.required],
+        content: ["",Validators.required]
+      });
     }
   }
- 
+
   submitClick() {
     if(this.mode == Mode.EDIT_MODE)
         this.editAction();
